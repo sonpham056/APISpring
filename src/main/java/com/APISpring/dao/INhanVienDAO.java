bce6066d3d8238717aae818755c6086ae6a9bc96
sonprolatao056 <57830932+sonprolatao056@users.noreply.github.com>
2021-05-09 21:41:33 +0700

    xong api nhanvien

--- src/main/java/com/APISpring/dao/INhanVienDAO.java
+++ src/main/java/com/APISpring/dao/INhanVienDAO.java
@@ -1,7 +1,13 @@
 package com.APISpring.dao;
 
+import java.util.List;
+
 import com.APISpring.entities.NhanVien;
 
 public interface INhanVienDAO extends IGeneric<NhanVien>{
 	NhanVien findById(String maNV);
+	List<NhanVien> findAll();
+	NhanVien saveNhanVien(NhanVien nhanVien);
+	NhanVien updateNhanVien(NhanVien nhanVien);
+	int deleteNhanVien(NhanVien nhanVien);
 }
--- src/main/java/com/APISpring/dao/imp/NhanVienDAO.java
+++ src/main/java/com/APISpring/dao/imp/NhanVienDAO.java
@@ -1,5 +1,7 @@
 package com.APISpring.dao.imp;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.APISpring.dao.INhanVienDAO;
@@ -11,6 +13,26 @@ public class NhanVienDAO extends AbstractDAO<NhanVien> implements INhanVienDAO{
 	@Override
 	public NhanVien findById(String maNV) {
 		return get("com.APISpring.entities.NhanVien", maNV);
+	}
+
+	@Override
+	public List<NhanVien> findAll() {
+		return getAll("NhanVien");
+	}
+
+	@Override
+	public NhanVien saveNhanVien(NhanVien nhanVien) {
+		return save(nhanVien);
+	}
+
+	@Override
+	public NhanVien updateNhanVien(NhanVien nhanVien) {
+		return update(nhanVien);
+	}
+
+	@Override
+	public int deleteNhanVien(NhanVien nhanVien) {
+		return delete(nhanVien);
 	}
 
 }
